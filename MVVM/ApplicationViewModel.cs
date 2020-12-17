cf5ec3f3c7c175571e9ec359d6f2f2d0f8d70a39
zinovik00@mail.ru <zinovik00@mail.ru>
2020-12-17 20:26:35 +0700

    Добавьте файлы проекта.

---
+++ MVVM/ApplicationViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MVVM
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private Function selectedFunction;
+        public ObservableCollection<Function> Functions { get; set; }
+        public double ResultAuto;
+        public List<int> Numbers;
+ 
+        public Function SelectedFunction
+        {
+            get { return selectedFunction; }
+            set
+            { 
+                selectedFunction = value;
+                OnPropertyChanged("SelectedFunction");
+            }
+        }
+        
+
+        public ApplicationViewModel()
+        {
+            List<int> Numbers = new List<int> { 1, 2, 3, 4, 5 };
+            Functions = new ObservableCollection<Function>
+            {
+                
+                new Function {Title="Линейная",VariableC=Numbers},
+                new Function {Title="квадратичная"},
+                new Function {Title="кубическая"},
+                new Function {Title="4-ой степени"},
+                new Function {Title="5-ой степени"}
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ MVVM/Function.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM
+{
+    public class Function : INotifyPropertyChanged
+    {
+        private string title;
+        private double variableA;
+        private double variableB;
+        private List<int> variableC;
+        private double variableX;
+        private int variableCNumber;
+        private double variableY;
+
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+        public double VariableA
+        {
+            get { return variableA; }
+            set
+            {
+                if (variableA != value) {
+                    variableA = value;
+                    OnPropertyChanged("VariableA");
+                    CalculateSum(); }
+            }
+        }
+        public double VariableB
+        {
+            get { return variableB; }
+            set
+            {
+                variableB = value;
+                OnPropertyChanged("VariableB");
+                CalculateSum();
+            }
+        }
+        public List<int> VariableC
+        {
+            get { return variableC; }
+            set
+            {
+                variableC = value;
+                OnPropertyChanged("VariableC");
+                CalculateSum();
+            }
+        }
+        public double VariableX
+        {
+            get { return variableX; }
+            set
+            {
+                variableX = value;
+                OnPropertyChanged("VariableX");
+                CalculateSum();
+            }
+        }
+        public int VariableCNumber
+        {
+            get { return variableCNumber; }
+            set
+            {
+                variableCNumber = value;
+                OnPropertyChanged("VariableCNumber");
+                CalculateSum();
+            }
+        }
+        public double VariableY
+        {
+            get { return variableY; }
+            set
+            {
+                variableY = value;
+                OnPropertyChanged("VariableY");
+                CalculateSum();
+            }
+        }
+        public double Result
+        {
+            get; set;
+        }
+
+
+              
+
+        private void CalculateSum()
+        {
+            switch (Title)
+            {
+                case "Линейная":
+                    Result = (VariableA * VariableX) + (VariableB * Math.Pow(VariableY, 0)) + VariableCNumber;
+                    OnPropertyChanged("Result");
+                    break;
+                case "квадратичная":
+                   Result = (VariableA * Math.Pow(VariableX, 2)) + (VariableB * Math.Pow(VariableY, 1)) + VariableCNumber;
+                    OnPropertyChanged("Result");
+                    break;
+                case "кубическая":
+                    Result = (VariableA * Math.Pow(VariableX, 3)) + (VariableB * Math.Pow(VariableY, 2)) +VariableCNumber;
+                    OnPropertyChanged("Result");
+                    break;
+                case "4-ой степен":
+                   Result = (VariableA * Math.Pow(VariableX, 4)) + (VariableB * Math.Pow(VariableY, 3)) + VariableCNumber;
+                    OnPropertyChanged("Result");
+                    break;
+                case "5-ой степени":
+                    Result = (VariableA * Math.Pow(VariableX, 5)) + (VariableB * Math.Pow(VariableY, 4)) + VariableCNumber;
+                    OnPropertyChanged("Result");
+                    break;
+
+            }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
